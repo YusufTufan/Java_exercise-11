299b3034f2103a0d7791303ae458b341c69a5369
Yusuf Tufan <yusufftufan@gamil.com>
2024-04-07 13:21:41 +0300

    Update ToplamKontrol.java

--- ToplamKontrol.java
+++ ToplamKontrol.java
@@ -6,14 +6,14 @@ public class ToplamKontrol {
         int number;
         
         System.out.println("Sayıları giriniz(Eğer -1 girerseniz toplama işlemi sona erecektir): ");
-        do{
-            System.out.print("Lütfen sayıyı tuşlayınız: ");
-             number = scanner.nextInt();
+        do{                                              //Do while seçme nedenimiz sağlanmadığı an direk terk etsin.
+            System.out.print("Lütfen sayıyı giriniz: ");
+             number = scanner.nextInt();                //Sayı istemi
 
             if (number != -1){
                 sum +=number;
             }
-        }while(number != -1);
+        }while(number != -1);                           //Her defasında -1 mi değil mi ?
         System.out.print("Sistem kapanana kadar giriş yapılan sayıların toplamı: "+sum);
         scanner.close();
     }
